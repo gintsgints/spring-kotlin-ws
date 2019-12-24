dc00e983be8a4c56edecc808643aaa465c31c4cf
Gints Polis <gints.polis@balcia.com>
2019-12-24 20:56:44 +0200

    Find One Fail if not found.

--- src/main/kotlin/com/balica/ws/springws3/greeting/GreetingController.kt
+++ src/main/kotlin/com/balica/ws/springws3/greeting/GreetingController.kt
@@ -1,9 +1,11 @@
 package com.balica.ws.springws3.greeting
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 import javax.validation.Valid
 
 @RestController
@@ -12,6 +14,12 @@ class GreetingController(private val repository: GreetingService) {
     @GetMapping("/")
     fun greeting() =
             repository.findTopBy()
+
+    @GetMapping("/{id}")
+    fun getGreeting(@PathVariable id: Long): GreetingEntity {
+        return repository.findById(id).orElse(null) ?:
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, """Greeting with ID:${id.toString()} not found""")
+    }
 
     @PostMapping("/", consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun createGreeting(@Valid @RequestBody greeting: GreetingEntity, result: BindingResult, model: Model): GreetingEntity {
