cf47e3351482d6ae77dfb78d7c58ff14dd05e9c8
Gints Polis <gints.polis@balcia.com>
2019-12-26 10:47:59 +0200

    Some integration tests.

--- src/test/kotlin/com/balica/ws/springws3/IntegrationTests.kt
+++ src/test/kotlin/com/balica/ws/springws3/IntegrationTests.kt
@@ -1,5 +1,6 @@
 package com.balica.ws.springws3
 
+import com.balica.ws.springws3.greeting.Greeting
 import com.balica.ws.springws3.greeting.GreetingEntity
 import org.assertj.core.api.Assertions.assertThat
 import org.hamcrest.CoreMatchers.equalTo
@@ -7,19 +8,37 @@ import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.skyscreamer.jsonassert.JSONAssert
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
-import org.springframework.boot.test.web.client.getForObject
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = ["spring.datasource.initialization-mode=always"])
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class IntegrationTests (@Autowired val restTemplate: TestRestTemplate) {
     @BeforeAll
     fun setup() {
         println(">> Setup")
+    }
+
+    @Test
+    fun `Insert greeting`() {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        val request = HttpEntity<Greeting>(Greeting("gr1", null), headers)
+        val result = restTemplate.postForEntity<Greeting>("/api/greeting/", request, Greeting::class.java)
+        assertThat(result.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun `GET greeting`() {
+        val result = restTemplate.getForEntity<Greeting>("/api/greeting/1", Greeting::class.java)
+        assertThat(result.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(result.body?.text).isEqualTo("gr1")
     }
 
     @AfterAll
---
+++ src/test/resources/data.sql
@@ -0,0 +1,6 @@
+DELETE FROM greeting_entity WHERE TRUE;
+
+INSERT INTO greeting_entity (id, text) VALUES (1, 'gr1');
+INSERT INTO greeting_entity (id, text) VALUES (2, 'gr2');
+INSERT INTO greeting_entity (id, text) VALUES (3, 'Greeting');
+INSERT INTO greeting_entity (id, text) VALUES (4, 'Greeting v2');
